62a76da25c39936f2b40a8db8de5a24dbd41003d
msyoki <msyokimutua@gmail.com>
2020-09-29 18:00:21 +0300

    add database URL

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://msyoki:psql20*@localhost/blog'
 
 class ProdConfig(Config):
     '''
